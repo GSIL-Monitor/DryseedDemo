a8b020fbd37c22df8aeae265047d56c40acdd6ad
caiminming <caiminming@jd.com>
2017-01-10 15:28:55 +0800

    nobug load images

--- app/src/main/java/com/dryseed/dryseedapp/Test.java
+++ app/src/main/java/com/dryseed/dryseedapp/Test.java
@@ -10,12 +10,21 @@ public class Test
 {
 	public static void main(String[] args)
 	{
-		// -0.5 ~ -0.7
+		/*// -0.5 ~ -0.7
 		for(int i=0; i<15; i++){
 			NumberFormat n = NumberFormat.getInstance();
 			n.setMaximumFractionDigits(1);
 			float f = Float.parseFloat(n.format(Math.random())) * 0.2f + 0.5f;
 			System.out.println(f);
-		}
+		}*/
+
+		String x = new String("dryseed");
+		change(x);
+		System.out.println(x);
 	}
+
+	public static void change(String x) {
+		x = "cai";
+	}
+
 }
