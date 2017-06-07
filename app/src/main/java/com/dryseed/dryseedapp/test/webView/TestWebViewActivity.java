5572cb1c89444bd35fb1d92c78a70c8efa37209c
caiminming <caiminming@jd.com>
2017-06-07 16:57:50 +0800

    fix memory leak

--- app/src/main/java/com/dryseed/dryseedapp/test/webView/TestWebViewActivity.java
+++ app/src/main/java/com/dryseed/dryseedapp/test/webView/TestWebViewActivity.java
@@ -3,6 +3,8 @@ package com.dryseed.dryseedapp.test.webView;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
@@ -60,10 +62,34 @@ public class TestWebViewActivity extends Activity {
             @Override
             public void run() {
                 Toast.makeText(TestWebViewActivity.this, "js调用了Native函数传递参数：" + str, Toast.LENGTH_SHORT).show();
-                String text = logTextView.getText() +  "\njs调用了Native函数传递参数：" + str;
+                String text = logTextView.getText() + "\njs调用了Native函数传递参数：" + str;
                 logTextView.setText(text);
             }
         });
+    }
 
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+            // 如果先调用destroy()方法，则会命中if (isDestroyed()) return;这一行代码，需要先onDetachedFromWindow()，再destory()
+            ViewParent parent = mWebView.getParent();
+            if (parent != null) {
+                ((ViewGroup) parent).removeView(mWebView);
+            }
+
+            mWebView.stopLoading();
+            // 退出时调用此方法，移除绑定的服务，否则某些特定系统会报错
+            mWebView.getSettings().setJavaScriptEnabled(false);
+            mWebView.clearHistory();
+            mWebView.clearView();
+            mWebView.removeAllViews();
+
+            try {
+                mWebView.destroy();
+            } catch (Throwable ex) {
+
+            }
+        }
+        super.onDestroy();
     }
 }
