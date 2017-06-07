ea3a7d3e7bb5a23ad5bbebf66e564f81dae06c09
caiminming <caiminming@jd.com>
2017-06-07 16:36:43 +0800

    nobug webview&js

---
+++ app/src/main/java/com/dryseed/dryseedapp/test/webView/TestWebViewActivity.java
@@ -0,0 +1,69 @@
+package com.dryseed.dryseedapp.test.webView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dryseed.dryseedapp.R;
+
+/**
+ * Created by caiminming on 2017/6/7.
+ */
+
+public class TestWebViewActivity extends Activity {
+
+    private WebView mWebView;
+    private TextView logTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview_layout);
+        mWebView = (WebView) findViewById(R.id.webview);
+        // 启用javascript
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        // 从assets目录下面的加载html
+        mWebView.loadUrl("file:///android_asset/demo.html");
+        mWebView.addJavascriptInterface(this, "wx");
+        logTextView = (TextView) findViewById(R.id.text);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                // 无参数调用
+                mWebView.loadUrl("javascript:actionFromNative()");
+                // 传递参数调用
+                mWebView.loadUrl("javascript:actionFromNativeWithParam(" + "'come from Native'" + ")");
+            }
+        });
+
+    }
+
+    @android.webkit.JavascriptInterface
+    public void actionFromJs() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(TestWebViewActivity.this, "js调用了Native函数", Toast.LENGTH_SHORT).show();
+                String text = logTextView.getText() + "\njs调用了Native函数";
+                logTextView.setText(text);
+            }
+        });
+    }
+
+    @android.webkit.JavascriptInterface
+    public void actionFromJsWithParam(final String str) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(TestWebViewActivity.this, "js调用了Native函数传递参数：" + str, Toast.LENGTH_SHORT).show();
+                String text = logTextView.getText() +  "\njs调用了Native函数传递参数：" + str;
+                logTextView.setText(text);
+            }
+        });
+
+    }
+}
