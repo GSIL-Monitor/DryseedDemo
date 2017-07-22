a53e653480de088f26aa0f4e2dad9f820e74f33f
caiminming <caiminming@jd.com>
2017-07-22 14:41:45 +0800

    nobug swordToOffer

--- app/src/main/java/com/dryseed/dryseedapp/LeetCode/sortAlgorithm/selectionSort/TestSelectionSort.java
+++
@@ -1,7 +0,0 @@
-package com.dryseed.dryseedapp.LeetCode.sortAlgorithm.selectionSort;
-
-/**
- * Created by caiminming on 2017/1/3.
- */
-public class TestSelectionSort {
-}
--- app/src/main/java/com/dryseed/dryseedapp/fresco/TestFrescoActivity.java
+++ app/src/main/java/com/dryseed/dryseedapp/fresco/TestFrescoActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.dryseed.dryseedapp.R;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 
@@ -20,9 +22,17 @@ public class TestFrescoActivity extends Activity {
 
         setContentView(R.layout.activity_fresco_layout);
         SimpleDraweeView img1 = (SimpleDraweeView) findViewById(R.id.fresco_eg1);
-
         Uri uri = Uri.parse("http://b.hiphotos.baidu.com/image/pic/item/e78.jpg");
         img1.setImageURI(uri);
+
+        /*SimpleDraweeView drawview = (SimpleDraweeView) findViewById(R.id.fresco_eg2);
+        DraweeController mDraweeController = Fresco.newDraweeControllerBuilder()
+                .setAutoPlayAnimations(true)
+                //加载drawable里的一张gif图
+                .setUri(Uri.parse("res://"+getPackageName()+"/"+R.drawable.test_1334_48))//设置uri
+                .build();
+        //设置Controller
+        drawview.setController(mDraweeController);*/
     }
 
     @Override
