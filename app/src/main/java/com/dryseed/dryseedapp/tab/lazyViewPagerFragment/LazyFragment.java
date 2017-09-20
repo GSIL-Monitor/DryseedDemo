5c99d082de540f07b516daed0abc51511f9f42ab
caiminming <caiminming@jd.com>
2017-09-20 11:16:33 +0800

    nobug LazyViewPagerFragment setUserVisibleHint

---
+++ app/src/main/java/com/dryseed/dryseedapp/tab/lazyViewPagerFragment/LazyFragment.java
@@ -0,0 +1,38 @@
+package com.dryseed.dryseedapp.tab.lazyViewPagerFragment;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by caiminming on 2017/9/20.
+ */
+
+public abstract class LazyFragment extends Fragment {
+    protected boolean isVisible;
+
+    /**
+     * 在这里实现Fragment数据的缓加载.
+     *
+     * @param isVisibleToUser
+     */
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (getUserVisibleHint()) {
+            isVisible = true;
+            onVisible();
+        } else {
+            isVisible = false;
+            onInvisible();
+        }
+    }
+
+    protected void onVisible() {
+        lazyLoad();
+    }
+
+    protected abstract void lazyLoad();
+
+    protected void onInvisible() {
+    }
+}
---
+++ app/src/main/java/com/dryseed/dryseedapp/tab/lazyViewPagerFragment/MainTab02.java
@@ -0,0 +1,121 @@
+package com.dryseed.dryseedapp.tab.lazyViewPagerFragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dryseed.dryseedapp.R;
+
+public class MainTab02 extends LazyFragment {
+
+    private static final String TAG = "MMM";
+
+    /**
+     * 标志位，标志已经初始化完成。
+     */
+    private boolean isPrepared;
+
+    /**
+     * 标记已加载完成，保证懒加载只能加载一次
+     */
+    private boolean hasLoaded = false;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Log.i(TAG, "MainTab02 onAttach");
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "MainTab02 onCreate");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "MainTab02 onCreateView");
+        View view = inflater.inflate(R.layout.activity_tab_viewpager_main_tab_01, container, false);
+
+        isPrepared = true;
+        lazyLoad();
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Log.i(TAG, "MainTab02 onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "MainTab02 onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i(TAG, "MainTab02 onDetach");
+        super.onDetach();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.i(TAG, "MainTab02 onDestroyView");
+        isPrepared = false;
+        hasLoaded = false;
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(TAG, "MainTab02 onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(TAG, "MainTab02 onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "MainTab02 onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i(TAG, "MainTab02 onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i(TAG, "MainTab02 onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        Log.i(TAG, "MainTab02 setUserVisibleHint : " + isVisibleToUser);
+        super.setUserVisibleHint(isVisibleToUser);
+    }
+
+    @Override
+    protected void lazyLoad() {
+        Log.i(TAG, "MainTab02 lazyLoad");
+        if(!isPrepared || !isVisible || hasLoaded) {
+            return;
+        }
+        Log.i(TAG, "MainTab02 lazyLoad do something");
+        hasLoaded = true;
+    }
+}
