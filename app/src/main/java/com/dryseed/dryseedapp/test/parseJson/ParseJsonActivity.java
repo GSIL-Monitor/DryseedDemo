d152ccfb2e960e1e560e27ac14e129385d239fe5
caiminming <caiminming@ztgame.com>
2017-12-20 01:24:54 +0800

    CollapsingToolbarLayout & parseJson

---
+++ app/src/main/java/com/dryseed/dryseedapp/test/parseJson/ParseJsonActivity.java
@@ -0,0 +1,59 @@
+package com.dryseed.dryseedapp.test.parseJson;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.dryseed.dryseedapp.BaseActivity;
+import com.dryseed.dryseedapp.MyApplication;
+import com.dryseed.dryseedapp.utils.ToastUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by caiminming on 2017/12/19.
+ */
+
+public class ParseJsonActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final String file = "error.json";
+        AssetManager assetManager = MyApplication.getInstance().getAssets();
+        InputStream is = null;
+        BufferedReader br = null;
+        try {
+            is = assetManager.open(file);
+            br = new BufferedReader(new InputStreamReader(is));
+            StringBuffer sb = new StringBuffer();
+            String str;
+            while ((str = br.readLine()) != null) {
+                sb.append(str);
+            }
+
+            JSONArray jsonArray = new JSONObject(sb.toString()).getJSONArray("server_code");
+            Log.d("MMM", jsonArray.toString());
+            ToastUtil.showToast(jsonArray.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dryseed/dryseedapp/widget/scrollingActivity/heyDemo/ScrollingActivity.java
@@ -0,0 +1,92 @@
+package com.dryseed.dryseedapp.widget.scrollingActivity.heyDemo;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.dryseed.dryseedapp.BaseActivity;
+import com.dryseed.dryseedapp.R;
+import com.dryseed.dryseedapp.utils.DPIUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by caiminming on 2017/12/19.
+ */
+
+public class ScrollingActivity extends BaseActivity {
+
+    @Bind(R.id.app_bar)
+    AppBarLayout mAppBarLayout;
+
+    @Bind(R.id.toolbar)
+    Toolbar mBToolBar;
+
+    @Bind(R.id.parallax)
+    FrameLayout mParallax;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hey_scrolling_layout);
+        ButterKnife.bind(this);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        int actionBar = getActionBarHeight(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+            int statusbar = DPIUtil.dip2px(25);
+            mBToolBar.getLayoutParams().height = actionBar + statusbar;
+            mBToolBar.setPadding(0, statusbar, 0, 0);
+        }
+
+        mAppBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                int max = mBToolBar.getHeight() - mParallax.getHeight(); // max = -657
+                Log.d("MMM", "verticalOffset : " + verticalOffset + " | max : " + max);
+
+                if (max != 0) {
+                    float offset = verticalOffset * 1f / max; //重合时offset=1，toolbar出现
+                    updateThemeColor(offset != 1);
+                }
+
+            }
+        });
+    }
+
+    private int getActionBarHeight(Context context) {
+        TypedArray array = context.getTheme().obtainStyledAttributes(new int[]{R.attr.actionBarSize});
+        int size = array.getDimensionPixelOffset(0, 0);
+        array.recycle();
+        return size;
+    }
+
+    private int getStatusBarColor(Context c) {
+        TypedArray array = c.getTheme().obtainStyledAttributes(new int[]{R.attr.defaultStatusBarColor,});
+        int mAccentColor = array.getColor(0, 0);
+        array.recycle();
+        return mAccentColor;
+    }
+
+    private void updateThemeColor(boolean isTransparent) {
+        if (isTransparent) {
+            mBToolBar.setBackgroundColor(Color.TRANSPARENT);
+        } else {
+            mBToolBar.setBackgroundColor(getStatusBarColor(this));
+        }
+    }
+}
