a145983d06abb7b5621c2c1afa35e21cbf08b91c
caiminming <caiminming@ztgame.com>
2017-12-26 20:54:01 +0800

    update glide version to 4.2.0

--- app/src/main/java/com/dryseed/dryseedapp/framework/glide/TestGlideActivity.java
+++ app/src/main/java/com/dryseed/dryseedapp/framework/glide/TestGlideActivity.java
@@ -1,18 +1,22 @@
 package com.dryseed.dryseedapp.framework.glide;
 
-import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 import com.dryseed.dryseedapp.BaseActivity;
 import com.dryseed.dryseedapp.R;
@@ -49,9 +53,9 @@ public class TestGlideActivity extends BaseActivity {
         String url = "http://p1.pstatp.com/large/166200019850062839d3"; //gif
         Glide.with(this)
                 .load(url)
-                .placeholder(R.drawable.ic_launcher)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .error(R.drawable.content)
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher))
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+                .apply(RequestOptions.errorOf(R.drawable.content))
                 .into(mImageView);
     }
 
@@ -77,15 +81,15 @@ public class TestGlideActivity extends BaseActivity {
         String url = "http://upload.jianshu.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144";
         Glide.with(this)
                 .load(url)
-                .placeholder(R.drawable.ic_launcher)
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .skipMemoryCache(true)
-                .error(R.drawable.content)
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher))
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+                .apply(RequestOptions.skipMemoryCacheOf(true))
+                .apply(RequestOptions.errorOf(R.drawable.content))
                 /*
                 无论你是否使用占位图，对于UI来说，图片的改变是相当大的一个动作。一个简单的方法可以让这个变化更平滑，更让人眼接受，这就是使用crossfade动画。Glide支持标准的crossfade动画，（对于目前版本3.6.1）是默认可用的。
                 crossFade()方法有另外一个特征：.crossFade(int duration),如果你想要减慢（或加快）动画，随便传入一个毫秒级的时间进去感受一下。默认的动画时间是300毫秒。
                  */
-                .crossFade(1000)
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
                 /*
                 如果你只是直接显示图片，而不需要crossfade效果，那就在Glide的请求构造里调用.dontAnimate()
                  */
@@ -97,18 +101,18 @@ public class TestGlideActivity extends BaseActivity {
         String url = "http://upload.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144"; // error url
         Glide.with(this)
                 .load(url)
-                .centerCrop()
-                .placeholder(R.drawable.ic_launcher)
-                .listener(new RequestListener<String, GlideDrawable>() {
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher))
+                .apply(RequestOptions.centerCropTransform())
+                .listener(new RequestListener<Drawable>() {
                     @Override
-                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
                         Log.d("MMM", "onException");
                         mImageView.setVisibility(View.GONE);
                         return false;
                     }
 
                     @Override
-                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
                         Log.d("MMM", "onResourceReady");
                         return false;
                     }
