8dde9b6c720b8884c459865e786a47d2fb0c80d9
caiminming <caiminming@ztgame.com>
2018-01-05 21:01:40 +0800

    glide4.0

---
+++ app/src/main/java/com/dryseed/dryseedapp/framework/glide/DsGlideModule.java
@@ -0,0 +1,58 @@
+package com.dryseed.dryseedapp.framework.glide;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool;
+import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.LruResourceCache;
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator;
+import com.bumptech.glide.module.AppGlideModule;
+import com.dryseed.dryseedapp.utils.StorageDirectoryUtil;
+
+/**
+ * https://muyangmin.github.io/glide-docs-cn/doc/generatedapi.html
+ */
+@com.bumptech.glide.annotation.GlideModule
+public class DsGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        MemorySizeCalculator calculator = new MemorySizeCalculator.Builder(context)
+                .setMemoryCacheScreens(3) //setMemoryCacheScreens设置MemoryCache应该能够容纳的像素值的设备屏幕数，说白了就是缓存多少屏图片，默认值是2。
+                .build();
+
+        int defaultMemoryCacheSize = calculator.getMemoryCacheSize();
+        int defaultBitmapPoolSize = calculator.getBitmapPoolSize();
+
+        int customMemoryCacheSize = (int) (1.2 * defaultMemoryCacheSize);
+        int customBitmapPoolSize = (int) (1.2 * defaultBitmapPoolSize);
+
+        builder.setMemoryCache(new LruResourceCache(customMemoryCacheSize));
+        builder.setBitmapPool(new LruBitmapPool(customBitmapPoolSize));
+
+        //磁盘缓存 Glide使用DiskLruCacheWrapper作为默认的磁盘缓存，默认大小是250M,缓存文件放在APP的缓存文件夹下。
+        builder.setDiskCache(new ExternalCacheDiskCacheFactory(context, StorageDirectoryUtil.getImageDirectory(), 250 * 1024 * 1024));
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide, Registry registry) {
+        super.registerComponents(context, glide, registry);
+    }
+
+    /**
+     * 为了维持对 Glide v3 的 GlideModules 的向后兼容性，Glide 仍然会解析应用程序和所有被包含的库中的 AndroidManifest.xml 文件，
+     * 并包含在这些清单中列出的旧 GlideModules 模块类。
+     * 如果你已经迁移到 Glide v4 的 AppGlideModule 和 LibraryGlideModule ，你可以完全禁用清单解析。
+     * 这样可以改善 Glide 的初始启动时间，并避免尝试解析元数据时的一些潜在问题。要禁用清单解析，请在你的 AppGlideModule 实现中复写 isManifestParsingEnabled() 方法
+     * https://muyangmin.github.io/glide-docs-cn/doc/configuration.html#appglidemodule
+     *
+     * @return
+     */
+    @Override
+    public boolean isManifestParsingEnabled() {
+        return false;
+    }
+}
--- app/src/main/java/com/dryseed/dryseedapp/framework/glide/TestGlideActivity.java
+++ app/src/main/java/com/dryseed/dryseedapp/framework/glide/TestGlideActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.engine.GlideException;
@@ -31,11 +32,10 @@ import butterknife.OnClick;
  * Created by User on 2017/7/23.
  */
 public class TestGlideActivity extends BaseActivity {
+    String url = "http://upload.jianshu.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144";
+
     @Bind(R.id.glide_image)
     ImageView mImageView;
-
-    @Bind(R.id.glide_button)
-    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,60 +44,141 @@ public class TestGlideActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.glide_button)
-    void onClick() {
-        loadImage5();
+    @OnClick(R.id.button1)
+    void onBtn1Click() {
+        loadImage7();
     }
 
+    @OnClick(R.id.button2)
+    void onBtn2Click() {
+        loadImage2();
+    }
+
+    @OnClick(R.id.button3)
+    void onBtn3Click() {
+        loadImage3();
+    }
+
+    @OnClick(R.id.button4)
+    void onBtn4Click() {
+        loadImage4();
+    }
+
+    /**
+     * 图片加载
+     */
     private void loadImage1() {
-        String url = "http://p1.pstatp.com/large/166200019850062839d3"; //gif
-        Glide.with(this)
-                .load(url)
-                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher))
-                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
-                .apply(RequestOptions.errorOf(R.drawable.content))
-                .into(mImageView);
+        String url = "http://i.imgur.com/DvpvklR.png";
+        Glide.with(this).load(url).into(mImageView);
+
+        // 加载本地图片
+        //File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Running.jpg");
+        //Glide.with(this).load(file).into(mImageView);
+
+        // 加载应用资源
+        //int resource = R.drawable.bg;
+        //Glide.with(this).load(resource).into(mImageView);
+
+        // 加载二进制流
+        //byte[] image = getImageBytes();
+        //Glide.with(this).load(image).into(mImageView);
+
+        // 加载URI
+        //String ANDROID_RESOURCE = "android.resource://";
+        //String FOREWARD_SLASH = "/";
+        //Uri uri = Uri.parse(ANDROID_RESOURCE + getPackageName() + FOREWARD_SLASH + R.drawable.ic_launcher);
+        //Glide.with(this).load(uri).into(mImageView);
     }
 
+    /**
+     * 占位符 渐现动画
+     */
     private void loadImage2() {
-        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "Running.jpg");
-        Glide.with(this)
-                .load(file)
-                .into(mImageView);
-    }
-
-    private void loadImage3() {
-        String ANDROID_RESOURCE = "android.resource://";
-        String FOREWARD_SLASH = "/";
-
-        Uri uri = Uri.parse(ANDROID_RESOURCE + getPackageName() + FOREWARD_SLASH + R.drawable.ic_launcher);
-
-        Glide.with(this)
-                .load(uri)
-                .into(mImageView);
-    }
-
-    private void loadImage4() {
         String url = "http://upload.jianshu.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144";
         Glide.with(this)
                 .load(url)
-                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher))
                 .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
                 .apply(RequestOptions.skipMemoryCacheOf(true))
+                .apply(RequestOptions.placeholderOf(R.drawable.ic_launcher))
                 .apply(RequestOptions.errorOf(R.drawable.content))
-                /*
-                无论你是否使用占位图，对于UI来说，图片的改变是相当大的一个动作。一个简单的方法可以让这个变化更平滑，更让人眼接受，这就是使用crossfade动画。Glide支持标准的crossfade动画，（对于目前版本3.6.1）是默认可用的。
-                crossFade()方法有另外一个特征：.crossFade(int duration),如果你想要减慢（或加快）动画，随便传入一个毫秒级的时间进去感受一下。默认的动画时间是300毫秒。
-                 */
                 .transition(DrawableTransitionOptions.withCrossFade(1000))
-                /*
-                如果你只是直接显示图片，而不需要crossfade效果，那就在Glide的请求构造里调用.dontAnimate()
-                 */
-                //.dontAnimate()
+                //.apply(RequestOptions.noAnimation())
                 .into(mImageView);
     }
 
+    /**
+     * 图片缩放和调整大小
+     */
+    private void loadImage3() {
+        String url = "http://upload.jianshu.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144";
+        Glide.with(this)
+                .load(url)
+                .apply(RequestOptions.overrideOf(500, 500)) //修改图片尺寸
+                //.apply(RequestOptions.centerCropTransform()) // centerCrop
+                //.apply(RequestOptions.fitCenterTransform()) // fitCenter
+                .into(mImageView);
+    }
+
+    /**
+     * 显示Gif
+     */
+    private void loadImage4() {
+        String url = "http://p1.pstatp.com/large/166200019850062839d3"; //gif
+        Glide.with(this)
+                //.asBitmap()
+                .asGif()
+                .load(url)
+                .apply(RequestOptions.errorOf(R.drawable.content))
+                .into(mImageView);
+    }
+
+    /**
+     * 缓存
+     */
     private void loadImage5() {
+        /*
+            DiskCacheStrategy.NONE 什么都不缓存，就像刚讨论的那样
+            DiskCacheStrategy.SOURCE 仅仅只缓存原来的全分辨率的图像。在我们上面的例子中，将会只有一个 1000x1000 像素的图片
+            DiskCacheStrategy.RESULT 仅仅缓存最终的图像，即，降低分辨率后的（或者是转换后的）
+            DiskCacheStrategy.ALL 缓存所有版本的图像（默认行为）
+        */
+        String url = "http://upload.jianshu.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144";
+        Glide.with(this)
+                .load(url)
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
+                .apply(RequestOptions.skipMemoryCacheOf(false))
+                .into(mImageView);
+    }
+
+
+    /**
+     * 请求优先级
+     */
+    private void loadImage6() {
+        /*
+            Priority.LOW
+            Priority.NORMAL
+            Priority.HIGH
+            Priority.IMMEDIATE
+         */
+        String url = "http://upload.jianshu.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144";
+        Glide.with(this)
+                .load(url)
+                .apply(RequestOptions.priorityOf(Priority.LOW))
+                .into(mImageView);
+    }
+
+    /**
+     * 缩略图
+     */
+    private void loadImage7() {
+        Glide.with(this)
+                .load(url)
+                .thumbnail(0.1f)
+                .into(mImageView);
+    }
+
+    private void loadImage() {
         String url = "http://upload.io/users/upload_avatars/1606996/bdb4c19752d3?imageMogr2/auto-orient/strip|imageView2/1/w/144/h/144"; // error url
         Glide.with(this)
                 .load(url)
