37ce959ba55ba8ba1d3806b9a3babcfbd56068b5
caiminming <caiminming@qiyi.com>
2018-07-01 23:41:26 +0800

    ImmersionBar

---
+++ app/src/main/java/com/dryseed/dryseedapp/widget/immersionBar/ImmersionBarActivity.java
@@ -0,0 +1,39 @@
+package com.dryseed.dryseedapp.widget.immersionBar;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.dryseed.dryseedapp.BaseActivity;
+import com.dryseed.dryseedapp.R;
+import com.dryseed.dryseedapp.widget.immersionBar.lib.ImmersionBar;
+
+/**
+ * @author CaiMinMing
+ *
+ * https://github.com/gyf-dev/ImmersionBar
+ */
+public class ImmersionBarActivity extends BaseActivity {
+    private ImmersionBar mImmersionBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immersion_bar_layout);
+        mImmersionBar = ImmersionBar.with(this);
+        mImmersionBar.init();
+
+        mImmersionBar.statusBarView(R.id.status_bar)
+                .navigationBarColor(R.color.colorPrimary)
+                .fullScreen(true)
+                .addTag("PicAndColor")  //给上面参数打标记，以后可以通过标记恢复
+                .init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != mImmersionBar) {
+            mImmersionBar.destroy();
+        }
+    }
+}
